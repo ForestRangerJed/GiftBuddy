53835092673386adb3e5635c56a9bfc99af1c870
Jared Eldridge <Jayrodeldridge@gmail.com>
2022-11-14 10:10:24 -0700

    Initial commit

---
+++ GiftBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftBuddyApp.swift
+//  GiftBuddy
+//
+//  Created by Jared Eldridge on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct GiftBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
